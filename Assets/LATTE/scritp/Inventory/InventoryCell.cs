b6017041bbce09668f5ec7ebdfe3dc64cd5ab0a3
tanwiziz <fguy17364@gmail.com>
2025-10-26 00:34:54 +0700

    er

--- Assets/LATTE/scritp/Inventory/InventoryCell.cs
+++
@@ -1,8 +0,0 @@
-﻿public enum CellState { Locked, Expand, Unlocked }
-
-[System.Serializable]
-public class InventoryCell
-{
-    public ItemInstance item;
-    public CellState state = CellState.Unlocked;
-}
